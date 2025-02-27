19023a9ba4ae31fd44eaa2c68e578a56e48e84b2
Schmidt1139 <mschmid4@umd.edu>
2025-02-26 21:52:30 -0500

    Adding polar-form equations of motion to propagation file

--- src/python/Propagation.py
+++ src/python/Propagation.py
@@ -1,5 +1,72 @@
 import numpy as np;
 
+def spacecraft_EOM_radial_2D_EB( self,t,y,params ):
+    
+    '''
+    ode propagation function
+    -----------------------------------------------------------------------------------
+    This is a set of equations of motion that govern a spacecraft in the 
+    2-dimensional problem using the polar form. 
+    
+    Inputs
+    -----------------------------------------------------------------------------------
+    t:      Elapsed time
+    y:      Input state vector (r,theta,r_dot,v_theta)
+    params: The list of parameters
+    
+    Outputs
+    -----------------------------------------------------------------------------------
+    Derivatives for state vector dy
+    '''
+    
+    
+    #get the set of parameters
+    params = params.astype(np.float32);
+    num_params = len(params);
+    
+    #check parameter length
+    if( num_params != 6 ):
+        raise Exception('Invalid number of parameters');
+    
+    #unpack the parameters
+    mu_cb = params[0];      #Central body gravitational parameter (km^3/s^2)
+    radius_cb = params[1];  #Central body radius (km)
+    C1 = params[2];         #Maximum spacecraft thrust
+    C2 = params[3];         #Specific impulse
+    u = params[4];          #Spacecraft throttle input (action/control)
+    beta = params[5];       #Spacecraft thrust angle
+    g0 = 9.80665;           #Gravity at sea level (m/s^2)
+    
+    #unpack state vector
+    r       = y[0];         #radius
+    theta   = y[1];         #theta
+    r_dot   = y[2];         #r_dot
+    v_theta = y[3];         #v_theta
+    m       = y[4];         #mass
+    
+    #calc derivatives
+    d_r         = r_dot;
+    d_theta     = v_theta / r;
+    d_r_dot     = v_theta**2 / r - mu_cb / r**2 + C1 / m * u * np.sin(beta);
+    d_v_theta   = - r_dot * v_theta / r + C1 / m * u * np.cos(beta);
+    d_m         = - C1 * u / g0 / C2; #kg/s
+    
+    #construct derivative array
+    dy = np.zeros(5, dtype = np.float32 );
+    
+    #assign derivatives if the spacecraft has not impacted central body, 
+    #otherwise keep derivatives at zero
+    if ( r > radius_cb ):
+        dy[0] = d_r;
+        dy[1] = d_theta;
+        dy[2] = d_r_dot;
+        dy[3] = d_v_theta;
+        dy[4] = d_m;
+    
+        
+    #return derivative array
+    return dy.astype(np.float32);
+
 #method for calculating spacecraft EOM for Hohmann transfer env   
 def spacecraft_EOM_f_2D_2B( self,t,y,params ):
     
