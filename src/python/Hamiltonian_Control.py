2af6ee0267f30eb2e6e38ab12f19e8364fa09773
Schmidt1139 <mschmid4@umd.edu>
2025-03-01 21:13:12 -0500

    Reframing init conditions to boundary conditions

--- src/python/Hamiltonian_Control.py
+++ src/python/Hamiltonian_Control.py
@@ -3,25 +3,37 @@ from TwoBody_Orb2Orb_Transfer_Env import *
 
 class Hamiltonian_Controller_TBT:
     
-    def extract_env_initial_conditions(self):
+    def extract_env_boundary_conditions(self):
+        
         r_0         = self.init_observation[0];
         theta_0     = self.init_observation[1];
         r_dot_0     = self.init_observation[2];
         v_theta_0   = self.init_observation[3];
         m_0         = self.init_observation[4];
+        mu          = self.init_observation[5];
+        r_f         = self.init_observation[6];
+        r_dot_f     = 0.0;
+        v_theta_f   = (mu/r_f) ** 0.5;
         
-        self.r_0 = r_0;
-        self.theta_0 = theta_0;
-        self.r_dot_0 = r_dot_0;
-        self.v_theta_0 = v_theta_0;
-        self.m_0 = m_0;
+        self.r_0            = r_0;
+        self.theta_0        = theta_0;
+        self.r_dot_0        = r_dot_0;
+        self.v_theta_0      = v_theta_0;
+        self.m_0            = m_0;
+        self.r_f            = r_f;
+        self.r_dot_f        = r_dot_f;
+        self.v_theta_f      = v_theta_f;
         
-        print("Initial Conditions");
+        
+        print("Boundary Conditions");
         print(f"R0: {r_0}");
         print(f"theta_0: {theta_0}");
         print(f"r_dot_0: {r_dot_0}");
         print(f"v_theta_0: {v_theta_0}");
         print(f"m_0: {m_0}");
+        print(f"r_f: {r_f}");
+        print(f"r_dot_f: {r_dot_f}");
+        print(f"v_theta_f: {v_theta_f}");
     
     def __init__(self, env: TwoBody_Orb2Orb_Transfer_Env, init_observation, 
                  init_info, input_TOF ):
@@ -32,7 +44,7 @@ class Hamiltonian_Controller_TBT:
         self.input_TOF = input_TOF;                 #User input time of flight [s]
         print("Hamiltonian targeter created\n");
         
-        self.extract_env_initial_conditions();
+        self.extract_env_boundary_conditions();
         
         
     
