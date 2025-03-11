deb7b756bece86377d2030079ede9d976e0f3c7e
Schmidt1139 <mschmid4@umd.edu>
2025-03-10 22:16:50 -0400

    Reducing scope of test script for HC
    The Hamiltonian control test script was reduced in scope down to basically initializing the Hamiltonian controller object and calling the "solution finder" method to run the logic implemented in the Hamiltonial control class. Until the issues with the solution finder are resolved the script will remain focused on just creating this controller object and calling the solution finder method.

--- src/scripts/test_Hamiltonians.py
+++ src/scripts/test_Hamiltonians.py
@@ -2,6 +2,7 @@
 import numpy as np;
 import gymnasium as gym;
 import matplotlib.pyplot as plot;
+import matplotlib.ticker as ticker
 import sys
 import os
 
@@ -14,6 +15,8 @@ sys.path.append(os.path.abspath("../python"))
 
 from Ephemeris import Ephemeris
 from Hamiltonian_Control import Hamiltonian_Controller_TBT
+from scipy.integrate import solve_ivp
+from Propagation import Hamiltonian_EOM_TBT_nd
 
 
 #register the environment if it isn't registered
@@ -36,84 +39,26 @@ num_traj = 1;
 input_TOF = 1.1 * 365.25 * 24 * 60 * 60;
 steps_per_traj = np.ceil( input_TOF / env.unwrapped.step_size );
 
+np.set_printoptions(precision=3)  # Limit to 3 decimal places
 
 
 def test_Hamiltonian_Solution( env, num_trajectories, num_steps_per_traj ):
     
-    count_traj = 0;
-    arr_episodes = np.array([]);
-    arr_reward_totals = np.array([]);
-    total_steps_in_env = 0;
-
-    for count_traj in range(0,num_traj):
-        
-        #reset the environment
-        et = 0.0;
-        steps = 0;
-        r_tot = 0.0;
+    eph = Ephemeris( );
+    init_observation, init_info = env.reset();
+    sun_rad = env.unwrapped.planet_radii[0];
+    C1 = init_info["max_thrust"]*1000; #max thrust in N
+    C2 = init_info["ISP"];
     
-        eph = Ephemeris( );
-        init_observation, init_info = env.reset();
-        
-        #compute Hamiltonian Solution
-        H_controller = Hamiltonian_Controller_TBT(env, init_observation, 
-                                                  init_info, input_TOF);
+    #compute Hamiltonian Solution
+    H_controller = Hamiltonian_Controller_TBT(env, init_observation, 
+                                              init_info, input_TOF);
     
     
-        while (steps < steps_per_traj):
-            
-            #Sample randomly from the action space. Since the action is a delta-V
-            #magnitude in km/s, and the action space is unbounded (-inf to inf) the
-            #test maneuver that is returned will be sampled from a Gaussian normal
-            #distribution with a mean of 0 and a standard deviation of 1. We 
-            #devide by 1000 in this test case to give relatively small maneuvers.
-            action = env.action_space.sample();
-            
-            observation, reward, terminated, truncated, info = env.step(action);
+    sol = H_controller.hamiltonian_solution_finder();
     
-            r_tot = r_tot + reward;
-    
-            elapsed_time = info['Elapsed time'];
-            a = info['a'];
-            e = info['e'];
-            
-            if ( terminated == True ):
-                break;
-            
-            eph.add_polar_data(elapsed_time, observation[0], observation[1], observation[2], observation[3] );
-            
-            #print( elapsed_time, a, e, reward );
-            steps = steps + 1;
-            
-                
-        arr_episodes = np.append(arr_episodes, count_traj);
-        arr_reward_totals = np.append(arr_reward_totals, r_tot);
-        
-        total_steps_in_env = total_steps_in_env + steps;
-            
-        print("Episode count: " + str(count_traj+1) + " of " + str(num_traj) + "   Total steps: " + str(steps) );
-        print("Total steps in environment: " + str(total_steps_in_env) );
-        
-        if (count_traj == num_traj-1):
-            print("Plotting last trajectory...");
-            fig = eph.plot_xy(info["planet_radii"]);
-            plot.show(fig);
-            
-     
-    fig_reward, ax = plot.subplots(figsize=(6, 6));
-    
-    ax.plot( arr_episodes, arr_reward_totals, label="Total Reward" );
-    
-    # Customize the figure
-    ax.set_title("Total Reward Per Episode");
-    ax.set_xlabel("Episode Count");
-    ax.set_ylabel("Total R");
-    ax.legend();
-    ax.grid(False);
-    plot.show(fig_reward);
-    
-            
-    print("Test successful");
+    arr_time    = sol.t;
+    variables   = sol.y;
 
 
 test_Hamiltonian_Solution(env, num_traj, steps_per_traj);
