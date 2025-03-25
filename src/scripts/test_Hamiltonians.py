47a9a32ebd449ebe2896e4ef0e40e3d7c3498130
Schmidt1139 <mschmid4@umd.edu>
2025-03-24 23:26:36 -0400

    Utilizing new ephem generation logic (instead of writing this in each test script)

--- src/scripts/test_Hamiltonians.py
+++ src/scripts/test_Hamiltonians.py
@@ -60,7 +60,34 @@ H_controller = Hamiltonian_Controller_TBT(env, init_observation,
                                           init_info, input_TOF);
 
 
-h_sol, eps, sol = H_controller.hamiltonian_solution_finder();
+#compute solution
+flag_solved, h_sol, eps, sol = H_controller.hamiltonian_solution_finder();
+
+#write output ephemeris
+eph_out, arr_time, arr_u, arr_rho, arr_alpha_x, arr_alpha_y = H_controller.generate_output_ephemeris(eph);
+
+fig, ax = plot.subplots(figsize=(6, 6));
+ax.plot( arr_time, arr_rho );
+ax.set_title("Switching Function");
+
+fig, ax = plot.subplots(figsize=(6, 6));
+ax.plot( arr_time, arr_u );
+ax.set_title("Spacecraft Thrust Throttle over Time");
+
+fig, ax = plot.subplots(figsize=(6, 6));
+ax.plot( arr_time, arr_alpha_x, label="alpha_x" );
+ax.plot( arr_time, arr_alpha_y, label="alpha_y" );
+ax.set_title("Alpha Vector (Maneuver Direction) over Time"); 
+ax.legend();  
+
+#Ephemeris plotting
+sun_rad = 6.957e8;
+sma_Earth   = 149598023 * 1000;             #m
+sma_Mars    = 2.32495e8 * 1000;             #m
+eph_out.plot_xy(sun_rad);
+eph_out.plot_xy_ref_orbit(sma_Earth, "Earth Orbit" );
+eph_out.plot_xy_ref_orbit(sma_Mars, "Mars Orbit" );
+
 
 np.set_printoptions(precision=16)
 print("Solution for initial co-states: ", h_sol);
