2da1a0c560a08046cb3144a16ddebe629b1bd007
Schmidt1139 <mschmid4@umd.edu>
2025-04-18 00:33:41 -0400

    Formatting script

--- src/scripts/solve_two_body_transfer_and_write_ephem.py
+++ src/scripts/solve_two_body_transfer_and_write_ephem.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import gymnasium as gym
 import matplotlib.pyplot as plot
@@ -14,48 +13,46 @@ sys.path.append(os.path.abspath("../python"))
 from Ephemeris import Ephemeris
 from Hamiltonian_Control import Hamiltonian_Controller_TBT
 
-#USER INPUTS ------------------------------------------------------------------
+# USER INPUTS ------------------------------------------------------------------
 
-#Initialize dictionary with input parameters to function
+# Initialize dictionary with input parameters to function
 args = {
-    "TOF": 1.1 * 365.25 * 24 * 60 * 60, #s
-    "r0": 2.32495e+08, #Initial state/radius [km]
-    "theta0": 0.0, #Initial state/true anomaly [rad]
-    "vr0": 0.0, #Initial state/radial velocity [km/s]
-    "vtheta0": 23.6464, #Initial state/transpose velocity [km/s]
-    "m0": 3366.0, #Initial state/mass [kg]
-    "mu": 1.3e+11, #Central body gravitational parameter [km^3/s^2]
-    "sma_target": 1.49598e+08, #Target sma of final circular orbit [km]
-    "max_thrust": 1.33, #Max thrust of the spacecraft engine [N]
-    "ISP": 3872.0 #Specific impulse of the spacecraft engine [s]
+    "TOF": 1.1 * 365.25 * 24 * 60 * 60,  # s
+    "r0": 2.32495e08,  # Initial state/radius [km]
+    "theta0": 0.0,  # Initial state/true anomaly [rad]
+    "vr0": 0.0,  # Initial state/radial velocity [km/s]
+    "vtheta0": 23.6464,  # Initial state/transpose velocity [km/s]
+    "m0": 3366.0,  # Initial state/mass [kg]
+    "mu": 1.3e11,  # Central body gravitational parameter [km^3/s^2]
+    "sma_target": 1.49598e08,  # Target sma of final circular orbit [km]
+    "max_thrust": 1.33,  # Max thrust of the spacecraft engine [N]
+    "ISP": 3872.0,  # Specific impulse of the spacecraft engine [s]
 }
 
-#Ephemeris filename
+# Ephemeris filename
 dir_ephemeris_out = "..\\..\\data\\training_ephems\\"
 filename_ephemeris_out = dir_ephemeris_out + "solution_ephemeris.txt"
-#END USER INPUTS --------------------------------------------------------------
+# END USER INPUTS --------------------------------------------------------------
 
-#register the environment if it isn't registered
-if ("TwoBody_Orb2Orb_Transfer_Env-v0" not in envs.registry.keys()):
-    
+# register the environment if it isn't registered
+if "TwoBody_Orb2Orb_Transfer_Env-v0" not in envs.registry.keys():
     register(
         id="TwoBody_Orb2Orb_Transfer_Env-v0",
         entry_point="TwoBody_Orb2Orb_Transfer_Env:TwoBody_Orb2Orb_Transfer_Env",
     )
-    
 
-def solve_two_body_transfer_and_write_ephem(env, filename_eph, args ):
 
-    #The prescribed time of flight for the transfer trajectory [s]
+def solve_two_body_transfer_and_write_ephem(env, filename_eph, args):
+    # The prescribed time of flight for the transfer trajectory [s]
     input_TOF = args["TOF"]
-    
+
     np.set_printoptions(precision=3)  # Limit to 3 decimal places
-    
-    #reset the TBT env
+
+    # reset the TBT env
     seed = 42
     init_observation, init_info = env.reset(seed=seed)
-    
-    #override init observation to input arguments
+
+    # override init observation to input arguments
     init_observation[0] = args["r0"]
     init_observation[1] = args["theta0"]
     init_observation[2] = args["vr0"]
@@ -63,62 +60,64 @@ def solve_two_body_transfer_and_write_ephem(env, filename_eph, args ):
     init_observation[4] = args["m0"]
     init_observation[5] = args["mu"]
     init_observation[6] = args["sma_target"]
-    init_info["max_thrust"] = args["max_thrust"]/1000
+    init_info["max_thrust"] = args["max_thrust"] / 1000
     init_info["ISP"] = args["ISP"]
-    
-    #ephemeris
-    eph = Ephemeris()
-    
-    print(init_observation)
-    
-    #extract some parameters of interest
-    sun_rad     = env.unwrapped.planet_radii[0]
 
-    #compute Hamiltonian Solution
-    H_controller = Hamiltonian_Controller_TBT(env, init_observation, 
-                                              init_info, input_TOF)
-    
-    
-    #compute solution
+    # ephemeris
+    eph = Ephemeris()
+
+    print(init_observation)
+
+    # extract some parameters of interest
+    sun_rad = env.unwrapped.planet_radii[0]
+
+    # compute Hamiltonian Solution
+    H_controller = Hamiltonian_Controller_TBT(
+        env, init_observation, init_info, input_TOF
+    )
+
+    # compute solution
     flag_solved, h_sol, eps, sol, log = H_controller.hamiltonian_solution_finder()
-    
-    #write output ephemeris
-    eph_out, arr_time, arr_u, arr_rho, arr_alpha_x, arr_alpha_y = H_controller.generate_output_ephemeris(eph)
-    
-    #plotting
+
+    # write output ephemeris
+    eph_out, arr_time, arr_u, arr_rho, arr_alpha_x, arr_alpha_y = (
+        H_controller.generate_output_ephemeris(eph)
+    )
+
+    # plotting
     fig, ax = plot.subplots(figsize=(6, 6))
-    ax.plot( arr_time, arr_rho )
+    ax.plot(arr_time, arr_rho)
     ax.set_title("Switching Function")
-    
+
     fig, ax = plot.subplots(figsize=(6, 6))
-    ax.plot( arr_time, arr_u )
+    ax.plot(arr_time, arr_u)
     ax.set_title("Spacecraft Thrust Throttle over Time")
-    
+
     fig, ax = plot.subplots(figsize=(6, 6))
-    ax.plot( arr_time, arr_alpha_x, label="alpha_x" )
-    ax.plot( arr_time, arr_alpha_y, label="alpha_y" )
-    ax.set_title("Alpha Vector (Maneuver Direction) over Time") 
-    ax.legend()  
-    
+    ax.plot(arr_time, arr_alpha_x, label="alpha_x")
+    ax.plot(arr_time, arr_alpha_y, label="alpha_y")
+    ax.set_title("Alpha Vector (Maneuver Direction) over Time")
+    ax.legend()
+
     fig, ax = plot.subplots(figsize=(6, 6))
-    ax.plot( arr_time, sol.y[4,:] )
-    ax.set_title("Spacecraft Mass Fraction over Time") 
-    
-    #Ephemeris plotting
+    ax.plot(arr_time, sol.y[4, :])
+    ax.set_title("Spacecraft Mass Fraction over Time")
+
+    # Ephemeris plotting
     sun_rad = 6.957e8
     eph_out.plot_xy(sun_rad)
-    eph_out.plot_xy_ref_orbit( args["sma_target"]*1000, "Target Orbit" )
-    
+    eph_out.plot_xy_ref_orbit(args["sma_target"] * 1000, "Target Orbit")
+
     np.set_printoptions(precision=16)
     print("Solution for initial co-states: ", h_sol)
     print("Final smoothing parameter used in solution generation: ", eps)
-    
-    #write ephemeris file
-    eph_out.write_to_file(filename_ephemeris_out, mod_vector_write_frequency=10 )
-    print("Ephemeris of trajectory written to: ", filename_ephemeris_out )
+
+    # write ephemeris file
+    eph_out.write_to_file(filename_ephemeris_out, mod_vector_write_frequency=10)
+    print("Ephemeris of trajectory written to: ", filename_ephemeris_out)
 
 
-#initialize the environment
+# initialize the environment
 env = gym.make("TwoBody_Orb2Orb_Transfer_Env-v0")
 
-solve_two_body_transfer_and_write_ephem(env, filename_ephemeris_out, args )
+solve_two_body_transfer_and_write_ephem(env, filename_ephemeris_out, args)
