157c61e44ae12e7668afd8d8abe139618934c7da
Schmidt1139 <mschmid4@umd.edu>
2025-04-18 00:33:25 -0400

    Formatting src files

--- src/python/Ephemeris.py
+++ src/python/Ephemeris.py
@@ -4,12 +4,11 @@ import os
 import time
 from datetime import datetime, timezone
 
-class Ephemeris():
-    
+
+class Ephemeris:
     def __init__(self):
-        
-        #initialize an empty ephemeris object
-        
+        # initialize an empty ephemeris object
+
         self.arr_et = np.array([])
         self.arr_x = np.array([])
         self.arr_y = np.array([])
@@ -20,134 +19,142 @@ class Ephemeris():
         self.arr_alpha_y = np.array([])
         self.arr_u = np.array([])
         self.num_vectors = 0
-        
-    def add_data(self,et,x,y,vx,vy,m,alpha_x,alpha_y,u):
-        
-        self.arr_et = np.append( self.arr_et, et )
-        self.arr_x = np.append( self.arr_x, x )
-        self.arr_y = np.append( self.arr_y, y )
-        self.arr_vx = np.append( self.arr_vx, vx )
-        self.arr_vy = np.append( self.arr_vy, vy )
-        self.arr_m = np.append( self.arr_m, m )
-        self.arr_alpha_x = np.append( self.arr_alpha_x, alpha_x )
-        self.arr_alpha_y = np.append( self.arr_alpha_y, alpha_y )
-        self.arr_u = np.append( self.arr_u, u )
+
+    def add_data(self, et, x, y, vx, vy, m, alpha_x, alpha_y, u):
+        self.arr_et = np.append(self.arr_et, et)
+        self.arr_x = np.append(self.arr_x, x)
+        self.arr_y = np.append(self.arr_y, y)
+        self.arr_vx = np.append(self.arr_vx, vx)
+        self.arr_vy = np.append(self.arr_vy, vy)
+        self.arr_m = np.append(self.arr_m, m)
+        self.arr_alpha_x = np.append(self.arr_alpha_x, alpha_x)
+        self.arr_alpha_y = np.append(self.arr_alpha_y, alpha_y)
+        self.arr_u = np.append(self.arr_u, u)
         self.num_vectors = self.num_vectors + 1
-        
-    def add_polar_data(self,et,r,theta,r_dot,v_theta,m,alpha_x,alpha_y,u):
-        
-        #convert polar coordinates to cartesian
+
+    def add_polar_data(self, et, r, theta, r_dot, v_theta, m, alpha_x, alpha_y, u):
+        # convert polar coordinates to cartesian
         x = r * np.cos(theta)
         y = r * np.sin(theta)
         vx = r_dot * np.cos(theta) - v_theta * r * np.sin(theta)
         vy = r_dot * np.sin(theta) + v_theta * r * np.cos(theta)
-        
-        self.arr_et = np.append( self.arr_et, et )
-        self.arr_x = np.append( self.arr_x, x )
-        self.arr_y = np.append( self.arr_y, y )
-        self.arr_vx = np.append( self.arr_vx, vx )
-        self.arr_vy = np.append( self.arr_vy, vy )
-        self.arr_m = np.append( self.arr_m, m )
-        self.arr_alpha_x = np.append( self.arr_alpha_x, alpha_x )
-        self.arr_alpha_y = np.append( self.arr_alpha_y, alpha_y )
-        self.arr_u = np.append( self.arr_u, u )
+
+        self.arr_et = np.append(self.arr_et, et)
+        self.arr_x = np.append(self.arr_x, x)
+        self.arr_y = np.append(self.arr_y, y)
+        self.arr_vx = np.append(self.arr_vx, vx)
+        self.arr_vy = np.append(self.arr_vy, vy)
+        self.arr_m = np.append(self.arr_m, m)
+        self.arr_alpha_x = np.append(self.arr_alpha_x, alpha_x)
+        self.arr_alpha_y = np.append(self.arr_alpha_y, alpha_y)
+        self.arr_u = np.append(self.arr_u, u)
         self.num_vectors = self.num_vectors + 1
-        
+
     def plot_xy(self, radius_central_body):
-        
         arr_x_cb = np.array([])
         arr_y_cb = np.array([])
-        
+
         max_x = max(abs(self.arr_x))
         max_y = max(abs(self.arr_y))
-        
-        max_lim = 1.1* max( [max_x,max_y] )
-        
+
+        max_lim = 1.1 * max([max_x, max_y])
+
         pts = 1000
-        
-        #plot central body
-        for i in range(0,pts):
-            
-            theta = 2*np.pi*i / pts
+
+        # plot central body
+        for i in range(0, pts):
+            theta = 2 * np.pi * i / pts
             x_cb = radius_central_body * np.cos(theta)
             y_cb = radius_central_body * np.sin(theta)
-            
-            arr_x_cb = np.append( arr_x_cb, x_cb)
-            arr_y_cb = np.append( arr_y_cb, y_cb)
-        
+
+            arr_x_cb = np.append(arr_x_cb, x_cb)
+            arr_y_cb = np.append(arr_y_cb, y_cb)
+
         fig, ax = plot.subplots(figsize=(6, 6))
-        
+
         ax.set_aspect("equal")
-        
-        #Get initial and final states
+
+        # Get initial and final states
         x0 = self.arr_x[0]
         y0 = self.arr_y[0]
         xf = self.arr_x[-1]
         yf = self.arr_y[-1]
-        
-        ax.plot( x0, y0, label="Initial State", marker='o', color='white', 
-                linestyle=None, markerfacecolor='blue', markeredgecolor='blue' )
-        ax.plot( xf, yf, label="Final State", marker='x', linestyle=None, 
-                markerfacecolor='black', markeredgecolor='black', color='white' )
-        ax.plot( self.arr_x, self.arr_y, label="Trajectory", color='blue' )
-        ax.plot( arr_x_cb, arr_y_cb, label="Central Body" )
-        
+
+        ax.plot(
+            x0,
+            y0,
+            label="Initial State",
+            marker="o",
+            color="white",
+            linestyle=None,
+            markerfacecolor="blue",
+            markeredgecolor="blue",
+        )
+        ax.plot(
+            xf,
+            yf,
+            label="Final State",
+            marker="x",
+            linestyle=None,
+            markerfacecolor="black",
+            markeredgecolor="black",
+            color="white",
+        )
+        ax.plot(self.arr_x, self.arr_y, label="Trajectory", color="blue")
+        ax.plot(arr_x_cb, arr_y_cb, label="Central Body")
+
         ax.set_title("Trajectory")
         ax.set_xlabel("X [km]")
         ax.set_ylabel("Y [km]")
-        ax.set_xlim([-max_lim,max_lim])
-        ax.set_ylim([-max_lim,max_lim])
+        ax.set_xlim([-max_lim, max_lim])
+        ax.set_ylim([-max_lim, max_lim])
         ax.legend()
         ax.grid(False)
-        
+
         self.fig_xy = fig
         self.ax_xy = ax
-        
+
         return fig
-    
-    def plot_xy_ref_orbit(self, orbit_sma, label ):
-        
+
+    def plot_xy_ref_orbit(self, orbit_sma, label):
         fig = self.fig_xy
         ax = self.ax_xy
-        
+
         arr_x_ref = np.array([])
         arr_y_ref = np.array([])
-        
+
         pts = 1000
-        
-        #plot central body
-        for i in range(0,pts):
-            
-            theta = 2*np.pi*i / pts
+
+        # plot central body
+        for i in range(0, pts):
+            theta = 2 * np.pi * i / pts
             x_ref = orbit_sma * np.cos(theta)
             y_ref = orbit_sma * np.sin(theta)
-            
-            arr_x_ref = np.append( arr_x_ref, x_ref)
-            arr_y_ref = np.append( arr_y_ref, y_ref)
-            
-        
-        ax.plot( arr_x_ref, arr_y_ref, label=label, linestyle='dashed' )
-        ax.legend(loc='upper left')
-        
+
+            arr_x_ref = np.append(arr_x_ref, x_ref)
+            arr_y_ref = np.append(arr_y_ref, y_ref)
+
+        ax.plot(arr_x_ref, arr_y_ref, label=label, linestyle="dashed")
+        ax.legend(loc="upper left")
+
         self.fig_xy = fig
         self.ax_xy = ax
-        
+
         return self.fig_xy
 
-    def write_to_file(self, file_path, mod_vector_write_frequency=1 ):
-        
+    def write_to_file(self, file_path, mod_vector_write_frequency=1):
         file_name_base = os.path.basename(file_path)
-        
+
         # Get generation time as UTC string
         time_generation = time.time()
-        string_time_generation_utc = datetime.fromtimestamp(time_generation, 
-                                                            tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f") 
-        
-        #Modified number of vectors
+        string_time_generation_utc = datetime.fromtimestamp(
+            time_generation, tz=timezone.utc
+        ).strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        # Modified number of vectors
         mod_num_vec = self.num_vectors // mod_vector_write_frequency
-        
-        with open( file_path, "w" ) as f:
-            
+
+        with open(file_path, "w") as f:
             header = (
                 "Astro Compass Ephemeris v 1.0\n"
                 f"File name: {file_name_base}\n"
@@ -167,15 +174,13 @@ class Ephemeris():
                 "\n"
                 "<Ephemeris Start>\n"
             )
-            
+
             f.write(header)
-            
-            for i in range(0,self.num_vectors-1):
-                
+
+            for i in range(0, self.num_vectors - 1):
                 modulo = i % mod_vector_write_frequency
-                
-                if ( modulo == 0 ):
-                    
+
+                if modulo == 0:
                     str_ephem_out = (
                         f"{self.arr_et[i]: .16e},"
                         f"{self.arr_x[i]: .16e},"
@@ -187,19 +192,11 @@ class Ephemeris():
                         f"{self.arr_alpha_y[i]: .16e},"
                         f"{self.arr_u[i]: .16e}"
                     )
-                    
+
                     f.write(str_ephem_out + "\n")
-            
+
             f.write("<Ephemeris End>\n")
-            
+
         f.close()
-        
+
         return f.closed
-            
-            
-        
-        
-        
-        
-        
-        
--- src/python/Spacecraft.py
+++ src/python/Spacecraft.py
@@ -1,108 +1,103 @@
 import numpy as np
 
+
 class Spacecraft:
-    
-    def __init__(self, r, theta, r_dot, v_theta, mass, C1, C2 ):
-        
-        #Initialize the state of the spacecraft
+    def __init__(self, r, theta, r_dot, v_theta, mass, C1, C2):
+        # Initialize the state of the spacecraft
         self.update_state(r, theta, r_dot, v_theta, mass)
-        
-        #Set propulsion parameters
-        self.max_thrust         = C1
-        self.specific_impulse   = C2
-        
-    def update_state(self,r,theta,r_dot,v_theta,mass):
-        
-        #Set state vector polar coordinates coordinates
+
+        # Set propulsion parameters
+        self.max_thrust = C1
+        self.specific_impulse = C2
+
+    def update_state(self, r, theta, r_dot, v_theta, mass):
+        # Set state vector polar coordinates coordinates
         self.r = r
         self.theta = theta
         self.r_dot = r_dot
         self.v_theta = v_theta
-        
-        #Set the spacecraft mass
+
+        # Set the spacecraft mass
         self.mass = mass
-        
-        #convert polar coordinates to cartesian
+
+        # convert polar coordinates to cartesian
         x = r * np.cos(theta)
         y = r * np.sin(theta)
         vx = r_dot * np.cos(theta) - v_theta * r * np.sin(theta)
         vy = r_dot * np.sin(theta) + v_theta * r * np.cos(theta)
-        
-        #Set state vector cartesian coordinates
+
+        # Set state vector cartesian coordinates
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
-        
-    def calc_Planar_OE(self,x_cb,y_cb,vx_cb,vy_cb,mu_cb):
-        
-        #determine coordinates relative to central body
+
+    def calc_Planar_OE(self, x_cb, y_cb, vx_cb, vy_cb, mu_cb):
+        # determine coordinates relative to central body
         x_rel = self.x - x_cb
         y_rel = self.y - y_cb
         vx_rel = self.vx - vx_cb
         vy_rel = self.vy - vy_cb
-        
-        #position and velocity magnitudes
-        r = ( x_rel**2 + y_rel**2 )**0.5
-        
-        #spacecraft position, vel, and z vectors
-        sc_pos = np.array([ x_rel, y_rel, 0.0 ])
-        sc_vel = np.array([ vx_rel, vy_rel, 0.0 ])
-        z_hat = np.array([ 1.0, 0.0, 0.0 ])
+
+        # position and velocity magnitudes
+        r = (x_rel**2 + y_rel**2) ** 0.5
+
+        # spacecraft position, vel, and z vectors
+        sc_pos = np.array([x_rel, y_rel, 0.0])
+        sc_vel = np.array([vx_rel, vy_rel, 0.0])
+        z_hat = np.array([1.0, 0.0, 0.0])
         r_hat = sc_pos / r
-        
-        #angular momentum
-        h_vec = np.cross( sc_pos, sc_vel )
+
+        # angular momentum
+        h_vec = np.cross(sc_pos, sc_vel)
         h = np.linalg.norm(h_vec)
         h_hat = h_vec / h
-        
-        #node line
-        N = np.cross( z_hat, h_hat )
+
+        # node line
+        N = np.cross(z_hat, h_hat)
         N_hat = N / np.linalg.norm(N)
-        
-        #eccentricity vector
-        e_vec = np.cross(sc_vel,h_vec) / mu_cb - sc_pos/r
+
+        # eccentricity vector
+        e_vec = np.cross(sc_vel, h_vec) / mu_cb - sc_pos / r
         e = np.linalg.norm(e_vec)
-        
-        #If e is zero, we will get an error dividing by zero, so the ecc vector
-        #is set at {0,0,0} if the magnitude is zero.
-        if ( e == 0.0 ):
-            e_hat = e_vec*0.0
+
+        # If e is zero, we will get an error dividing by zero, so the ecc vector
+        # is set at {0,0,0} if the magnitude is zero.
+        if e == 0.0:
+            e_hat = e_vec * 0.0
         else:
-            e_hat = e_vec/e 
-        
-        #semi major axis
-        rp = h**2 / mu_cb / ( 1 + e * np.cos(0) )
-        ra = h**2 / mu_cb / ( 1 + e * np.cos( np.pi ) )
-        a = 1/2 * ( rp + ra )
-        
-        #argument of periapsis
-        if ( e_vec[2] >= 0.0 ):
-            w = np.acos( np.dot( N_hat, e_hat ) )
+            e_hat = e_vec / e
+
+        # semi major axis
+        rp = h**2 / mu_cb / (1 + e * np.cos(0))
+        ra = h**2 / mu_cb / (1 + e * np.cos(np.pi))
+        a = 1 / 2 * (rp + ra)
+
+        # argument of periapsis
+        if e_vec[2] >= 0.0:
+            w = np.acos(np.dot(N_hat, e_hat))
         else:
-            w = 2 * np.pi - np.acos( np.dot( N_hat, e_hat ) )
-        
-        #true anomaly - extra error handling included,
-        #mainly needed for hyperbolic instances
-        if ( np.dot( sc_pos, sc_vel ) >= 0.0 ):
-            
-            dotp = np.dot(e_hat,r_hat)
-            if (dotp < -1 ):
+            w = 2 * np.pi - np.acos(np.dot(N_hat, e_hat))
+
+        # true anomaly - extra error handling included,
+        # mainly needed for hyperbolic instances
+        if np.dot(sc_pos, sc_vel) >= 0.0:
+            dotp = np.dot(e_hat, r_hat)
+            if dotp < -1:
                 dotp = -1
-            elif (dotp > 1):
-                dotp = 1    
-                
-            theta = np.acos( dotp )
-                
-        else:
-            
-            #check acos domain
-            dotp = np.dot(e_hat,r_hat)
-            if (dotp < -1 ):
-                dotp = -1
-            elif (dotp > 1):
+            elif dotp > 1:
                 dotp = 1
-                
-            theta = 2 * np.pi - np.acos( dotp )
-        
+
+            theta = np.acos(dotp)
+
+        else:
+            # check acos domain
+            dotp = np.dot(e_hat, r_hat)
+            if dotp < -1:
+                dotp = -1
+            elif dotp > 1:
+                dotp = 1
+
+            theta = 2 * np.pi - np.acos(dotp)
+
         return a, e, w, theta
