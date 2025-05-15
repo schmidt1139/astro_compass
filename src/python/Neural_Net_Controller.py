ac78c09df8debe34d7f4cbbb7a268d6e2765d28e
Schmidt1139 <mschmid4@umd.edu>
2025-05-14 23:05:52 -0400

    Adding basic neural network controller class

---
+++ src/python/Neural_Net_Controller.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class NN_TBT_Controller(nn.Module):
+    def __init__(self):
+        super(NN_TBT_Controller, self).__init__()
+        print("Neural Network Controller Created")
+        
+        self.fc1 = nn.Linear( 4, 32 ) #input layer
+        self.fc2 = nn.Linear( 32, 32 ) #hidden layer 1
+        self.fc3 = nn.Linear( 32, 32 ) #hidden layer 2
+        self.fc4 = nn.Linear( 32, 32 ) #hidden layer 3
+        self.fc5 = nn.Linear( 32, 32 ) #hidden layer 4
+        self.fc6 = nn.Linear( 32, 3 ) #output layer
+        
+        
+    def forward(self,x):
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        x = torch.tanh(self.fc3(x))
+        x = torch.tanh(self.fc4(x))
+        x = torch.tanh(self.fc5(x))
+        x = self.fc6(x)
+        
+        return x
+        
