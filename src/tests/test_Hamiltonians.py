064ed0b7b6de1a59e9ef873391f599530a778ec6
Schmidt1139 <mschmid4@umd.edu>
2025-06-04 22:48:16 -0400

    Adding second section to test Hamiltonian controller both with and without live reporting

--- src/tests/test_Hamiltonians.py
+++ src/tests/test_Hamiltonians.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gymnasium as gym
+import matplotlib
 import matplotlib.pyplot as plot
 import sys
 import os
@@ -61,7 +62,8 @@ init_observation, init_info = env.reset(seed=seed)
 # ephemeris
 eph = Ephemeris()
 
-print(init_observation)
+print("Test Hamiltonian Controller\n")
+print("Initial observation vector: ", init_observation)
 
 # extract some parameters of interest
 sun_rad = env.unwrapped.planet_radii[0]
@@ -69,29 +71,51 @@ C1 = init_info["max_thrust"] * 1000  # max thrust in N
 C2 = init_info["ISP"]  # spacecraft specific impulse in seconds
 
 # compute Hamiltonian Solution
-H_controller = Hamiltonian_Controller_TBT(env, init_observation, init_info, input_TOF)
+H_controller = Hamiltonian_Controller_TBT(env, init_observation, init_info, input_TOF, flag_report_live=True)
 
 
 # compute solution
 flag_solved, h_sol, eps, sol, log = H_controller.hamiltonian_solution_finder()
+
+for item in log:
+    print(item)
 
 # write output ephemeris
 eph_out, arr_time, arr_u, arr_rho, arr_alpha_x, arr_alpha_y = (
     H_controller.generate_output_ephemeris(eph)
 )
 
+# compute Hamiltonian Solution
+H_controller2 = Hamiltonian_Controller_TBT(env, init_observation, init_info, input_TOF)
+
+# compute solution
+flag_solved, h_sol, eps, sol, log = H_controller2.hamiltonian_solution_finder()
+
+for item in log:
+    print(item)
+
+# write output ephemeris
+eph_out2, arr_time2, arr_u2, arr_rho2, arr_alpha_x2, arr_alpha_y2 = (
+    H_controller2.generate_output_ephemeris(eph)
+)
+
 fig, ax = plot.subplots(figsize=(6, 6))
 ax.plot(arr_time, arr_rho)
+ax.plot(arr_time2, arr_rho2)
+fig.tight_layout()
 ax.set_title("Switching Function")
 
 fig, ax = plot.subplots(figsize=(6, 6))
 ax.plot(arr_time, arr_u)
+ax.plot(arr_time2, arr_u2)
+fig.tight_layout()
 ax.set_title("Spacecraft Thrust Throttle over Time")
 
 fig, ax = plot.subplots(figsize=(6, 6))
 ax.plot(arr_time, arr_alpha_x, label="alpha_x")
 ax.plot(arr_time, arr_alpha_y, label="alpha_y")
 ax.set_title("Alpha Vector (Maneuver Direction) over Time")
+fig.tight_layout()
 ax.legend()
 
 # Ephemeris plotting
@@ -112,3 +136,6 @@ for item in log:
     print(item)
 
 print(sol)
+
+
+eph_out2.write_to_file("data\\test_data\\test_H_ephem.txt")
