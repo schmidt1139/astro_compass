9738f29665a60a24f10ecbe2e321c1b2dbb232fa
Michael Schmidt <mschmid4@umd.edu>
2025-07-02 02:13:43 -0400

    Boilerplate updates/moved plot formatting

--- src/scripts/train_neural_network.py
+++ src/scripts/train_neural_network.py
@@ -1,13 +1,11 @@
-# %%
 import sys
 import os
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import gymnasium as gym
+import matplotlib.pyplot as plt
 
 # Adding python src code directory
 current_dir = os.path.dirname(__file__)
@@ -22,29 +20,8 @@ from Neural_Net_Controller import NN_TBT_Controller
 from Training_Data_Generation import read_ephems_from_dir
 from StateVectorUtilities import non_dimensionalize
 from Constants import Constants
-from Ephemeris import Ephemeris
-
-
-
-# plotting setup
-matplotlib.rcParams.update(
-    {
-        "text.usetex": False,  # Use LaTeX for all text
-        "font.family": "serif",  # Use serif font
-        "font.size": 10,  # Match AIAA body font size
-        "axes.labelsize": 10,
-        "axes.titlesize": 10,
-        "legend.fontsize": 9,
-        "xtick.labelsize": 9,
-        "ytick.labelsize": 9,
-        "lines.linewidth": 1.2,
-        "lines.markersize": 4,
-        "figure.figsize": (3.5, 2.5),  # Single-column figure
-        "figure.dpi": 300,
-        "savefig.bbox": "tight",
-        "axes.grid": False,  # No gridlines in AIAA style
-    }
-)
+from Plotting_Utils import format_plots, plot_training_loss
+from NN_Utils import evaluate_neural_network, pre_process_training_data, training_epoch
 
 # register the environment if it isn't registered
 if "TwoBody_Orb2Orb_Transfer_Env-v0" not in envs.registry.keys():
