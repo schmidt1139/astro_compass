4ebcb6eb35d518962b4b1b5352132f4c99847d28
Michael Schmidt <mschmid4@umd.edu>
2025-11-28 00:01:18 -0500

    add data path from config

--- src/scripts/pretrain/generate_buffer.py
+++ src/scripts/pretrain/generate_buffer.py
@@ -90,10 +90,9 @@ def main(params, training_data, seed_in=42):
 
 if __name__ == "__main__":
     config_toml = "SAC_training_TBR_polar__JM_config.toml"
-    training_data = "C:\\Users\\micha\\MSI_Data\\Masters_Thesis\\z_script_output\\training_TBR_overfit\\ephems"
 
     config_toml = "gen_buffer_config.toml"
     path_config = os.path.join(PROJECT_ROOT, "data", "config", config_toml)
     params = read_toml_config_file(path_config)
-
+    training_data = params["path_training_data"]
     main(params, training_data)
