a5b07f580240b2f45e4969b0a30a049fb4ea8476
Michael Schmidt <mschmid4@umd.edu>
2025-11-29 19:20:11 -0500

    added position rollout plot

--- src/python/utils/eval_utils.py
+++ src/python/utils/eval_utils.py
@@ -40,6 +40,7 @@ def mc_evaluate_agent(params):
     arr_position_res = []
     arr_velocity_res = []
     arr_m = []
+    list_pos_residuals = []
 
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
         futures = [executor.submit(run_single_rollout, s, params) for s in seeds]
@@ -59,12 +60,13 @@ def mc_evaluate_agent(params):
         arr_position_res.append(rollout_data.arr_position_res[-1])
         arr_velocity_res.append(rollout_data.arr_velocity_res[-1])
         arr_m.append(rollout_data.arr_mass[-1])
+        list_pos_residuals.append(rollout_data.arr_position_res)
 
-    return arr_episode_numbers, arr_episode_rs, arr_position_res, arr_velocity_res, arr_m
+    return arr_episode_numbers, arr_episode_rs, arr_position_res, arr_velocity_res, arr_m, list_pos_residuals
 
 def plot_log_mc_results(mc_results, test_log, params):
 
-    arr_episode_numbers, arr_episode_rs, arr_position_res, arr_velocity_res, arr_m = mc_results
+    arr_episode_numbers, arr_episode_rs, arr_position_res, arr_velocity_res, arr_m, list_pos_residuals = mc_results
 
     mean_r = sum(arr_episode_rs) / len(arr_episode_rs)
     percentile_95_r = np.percentile(arr_episode_rs, 95)
@@ -144,6 +146,19 @@ def plot_log_mc_results(mc_results, test_log, params):
     plt.grid(True)
     plt_path = params["path_plots"]
     plt.savefig(f"{plt_path}/mc_episode_final_mass_histogram.png")
+    plt.close()
+
+    #plot position residuals over time for all episodes
+    plt.figure(figsize=(10, 6))
+    for i, pos_residuals in enumerate(list_pos_residuals):
+        plt.plot(pos_residuals, alpha=0.5)
+
+    plt.title('Position Residuals Over Time for ' + str(len(list_pos_residuals)) + ' Episodes')
+    plt.xlabel('Time Step')
+    plt.ylabel('Position Residual (nd)')
+    plt.grid(True)
+    plt_path = params["path_plots"]
+    plt.savefig(f"{plt_path}/mc_position_residuals_over_time.png")
     plt.close()
 
     return test_log
