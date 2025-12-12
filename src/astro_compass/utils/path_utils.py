d2348bbda248edc9b1d15112a41fb94e330dce66
John M <john.r.martin@colorado.edu>
2025-12-11 19:05:23 -0500

    Add config root to path utils

--- src/astro_compass/utils/path_utils.py
+++ src/astro_compass/utils/path_utils.py
@@ -4,3 +4,4 @@ import astro_compass
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(astro_compass.__file__)) + "/../.."
 DATA_ROOT = os.path.join(PROJECT_ROOT, "data")
+CONFIG_ROOT = os.path.join(PROJECT_ROOT, "data", "config")
