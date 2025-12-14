4f3a8a7f3146040d0830449282167f47977b6ed7
John M <john.r.martin@colorado.edu>
2025-12-13 19:22:12 -0500

    Pull out rollout from eval script

--- scripts/TBT/eval_TBT.py
+++ scripts/TBT/eval_TBT.py
@@ -6,7 +6,6 @@ import torch
 from astro_compass.constants.constants import Constants
 from astro_compass.core.ephemeris import Ephemeris as Ephemeris
 from astro_compass.core.hamiltonian_control import Hamiltonian_Controller_TBT
-from astro_compass.core.spacecraft import Spacecraft
 from astro_compass.utils.env_utils import gen_rl_environment
 from astro_compass.utils.log_utils import (
     read_toml_config_file,
@@ -17,8 +16,7 @@ from astro_compass.utils.path_utils import (
     RUNS_ROOT,
     get_run_paths,
 )
-from astro_compass.utils.plotting_utils import SACRolloutData, plot_SAC_training
-from astro_compass.utils.state_vector_utils import cartesian_to_polar
+from astro_compass.utils.plotting_utils import plot_SAC_training
 
 plt.style.use("data/support_files/light_paper.mplstyle")
 print("GPU available: ", torch.cuda.is_available())
@@ -74,92 +72,6 @@ def eval_TBT_agent(env, model, params, path_output, path_ephems):
 
     print("Complete!")
     print("Plots saved to: " + path_output)
-
-
-def rollout_policy(
-    env,
-    model,
-    params,
-    sma_t_i,
-    obs,
-    info,
-    count_step,
-    flag_continue,
-):
-    rollout_data1 = SACRolloutData()
-    eph = Ephemeris()
-    count_step = 0
-    flag_continue = True
-    terminated = False
-    truncated = False
-    while flag_continue:
-        # step the env
-        action, _states = model.predict(obs, deterministic=True)
-        throttle = action[0]
-        alpha_x = action[1]
-        alpha_y = action[2]
-
-        # dim state
-        t_i = info["Elapsed time"]
-        t_i_days = t_i / (3600 * 24)
-        x_i = obs[0] * params["l_star"]
-        y_i = obs[1] * params["l_star"]
-        vx_i = obs[2] * params["l_star"] / params["t_star"]
-        vy_i = obs[3] * params["l_star"] / params["t_star"]
-        m_i = obs[4] * params["m_star"]
-
-        # log data to ephemeris
-        eph.add_data(t_i, x_i, y_i, vx_i, vy_i, m_i, alpha_x, alpha_y, throttle)
-
-        # create polar state, create a temp SC object and calc OE
-        r_i, theta_i, rdot_i, vtheta_i = cartesian_to_polar(x_i, y_i, vx_i, vy_i)
-        SC = Spacecraft(
-            r_i, theta_i, rdot_i, vtheta_i, m_i, params["max_T"], params["ISP"]
-        )
-        arr_OE = SC.calc_Planar_OE(0.0, 0.0, 0.0, 0.0, params["mu"])
-
-        obs, reward, terminated, truncated, info = env.step(action)
-        reward_mass_component = info.get("reward_mass_component", 0.0)
-        reward_distance_component = info.get("reward_distance_component", 0.0)
-
-        count_step = count_step + 1
-
-        # log data
-        rollout_data1.add_step(
-            t_i_days,
-            reward,
-            throttle,
-            alpha_x,
-            alpha_y,
-            obs[0],
-            obs[1],
-            obs[2],
-            obs[3],
-            arr_OE[0],
-            sma_t_i,
-            arr_OE[1],
-            0.0,
-            1.0,
-            reward_mass_component,
-            reward_distance_component,
-        )
-
-        if terminated or truncated:
-            break
-
-    print("Test trajectory complete")
-    print("Steps taken: " + str(count_step))
-    print("Total reward: " + str(rollout_data1.sum_reward))
-    print("Final x: " + str(obs[0]) + " ")
-    print("Final y: " + str(obs[1]) + " ")
-    print("Final vx: " + str(obs[2]) + " ")
-    print("Final vy: " + str(obs[3]) + " ")
-    print("Final m: " + str(obs[4]) + " ")
-    print("Final sma: " + str(arr_OE[0]) + " ")
-    print("Final ecc: " + str(arr_OE[1]) + " ")
-    print("terminated: " + str(terminated) + " ")
-    print("truncated: " + str(truncated) + " ")
-    return rollout_data1, eph
 
 
 def generate_hamiltonian_trajectory(env, params, path_output, path_ephems, obs, info):
