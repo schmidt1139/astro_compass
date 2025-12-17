a923d0178d549448104fcc84c4d8c3641c24eb04
John M <john.r.martin@colorado.edu>
2025-12-17 16:38:50 -0500

    Update plot ephems to load from pickles

--- scripts/visualization/plot_ephems.py
+++ scripts/visualization/plot_ephems.py
@@ -1,22 +1,13 @@
 import os
+import pickle
 
-from astro_compass.core.ephemeris import Ephemeris
-from astro_compass.utils.path_utils import PROJECT_ROOT
+from astro_compass.utils.path_utils import DATA_ROOT
 from astro_compass.vis.ephem_plotter import EphemPlotter
 
 
-def ingest_ephem_and_plot(path_to_ephemeris):
-    print("Ingest Ephemeris and Plot Script")
-    print("Ephemeris path: ", path_to_ephemeris)
-
-    # create an ephemeris object
-    eph = Ephemeris()
-
-    # read in data from the specified file
-    eph.read(path_to_ephemeris)
-
-    print("Successfully ingested ephemeris")
-    print("Number of vectors: ", eph.num_vectors)
+def main(path_to_ephemeris):
+    with open(path_to_ephemeris, "rb") as f:
+        eph = pickle.load(f)
 
     # Plot the ephemeris
     vis = EphemPlotter(eph)
@@ -28,8 +19,8 @@ def ingest_ephem_and_plot(path_to_ephemeris):
     print("Final vector: ", state_vector)
 
 
-directory = os.path.join(PROJECT_ROOT, "z_script_output", "temp_out")
-ephem_file_name = "test_TBR_ephem_traj_seed_2002693622_tof_1p0_scenario_0_extended.txt"
-path_to_ephemeris = directory + ephem_file_name
+if __name__ == "__main__":
+    directory = os.path.join(DATA_ROOT, "pre-training-data", "TBT", "pickle")
+    path_to_ephemeris = os.path.join(directory, "8505947.pkl")
 
-ingest_ephem_and_plot(path_to_ephemeris)
+    main(path_to_ephemeris)
