1b973fcf55daaa2141b97cf1b31d34cbc8f4d3ef
John M <john.r.martin@colorado.edu>
2025-12-17 21:58:22 -0500

    Update rollout vis script

--- scripts/visualization/plot_rollout_metrics.py
+++ scripts/visualization/plot_rollout_metrics.py
@@ -1,27 +1,37 @@
 import os
+import pickle
 
-from astro_compass.utils.plot_utils import RUNS_ROOT
+import matplotlib.pyplot as plt
 
 from astro_compass.core.ephem_converter import generate_rollouts
+from astro_compass.utils.log_utils import read_toml_config_file
+from astro_compass.utils.path_utils import CONFIG_ROOT, DATA_ROOT, PLOT_ROOT
 from astro_compass.vis.rollout_plotter import RolloutPlotter
 
 
 def main():
-    rollout_data = None
-    path = None
-
     # Generate rollout data from the Hamiltonian Trajectories
+    path_config = os.path.join(CONFIG_ROOT, "SAC_training_TBT_config.toml")
+    params = read_toml_config_file(path_config)
 
     # Load ephem pickles
-    directory = os.path.join(RUNS_ROOT, "20251214_175440")
-    ephem = import_ephem(directory, idx=0)
-    rollout = generate_rollouts(ephem)
+    ephem_path = os.path.join(
+        DATA_ROOT,
+        "pre-training-data",
+        "TBT",
+        "pickle",
+        "477753.pkl",
+    )
+    with open(ephem_path, "rb") as f:
+        ephem = pickle.load(f)
+
+    rollouts = generate_rollouts(ephem, params)
 
     # Plot the rollout metrics vs time
 
-    vis = RolloutPlotter(rollout_data, path)
+    vis = RolloutPlotter(rollouts[0], PLOT_ROOT)
     vis.plot()
-    pass
+    plt.show()
 
 
 if __name__ == "__main__":
